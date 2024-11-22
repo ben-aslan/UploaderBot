316172e7e54d7d2280a9078a1cfc0c14dbbe8167
Taha-Hasemi <kiziltahaaslan@gmail.com>
2024-11-22 15:42:59 +0330

    change: stepindex

--- TelegramBotCore/Processes/ChannelVideoProcess.cs
+++ TelegramBotCore/Processes/ChannelVideoProcess.cs
@@ -5,7 +5,7 @@ using TelegramBotCore.Processes.Abstract;
 
 namespace TelegramBotCore.Processes;
 
-[Process(ChatTypeId = (int)ChatType.Group, StepId = (int)EStep.GroupVideoFirstAlgorithm, StepIndexId = (int)EStep.Home)]
+[Process(ChatTypeId = (int)ChatType.Supergroup, StepId = (int)EStep.GroupVideoFirstAlgorithm, StepIndexId = (int)EStep.Home)]
 public class ChannelVideoProcess : IProcess
 {
     public void Execute(Update update)
