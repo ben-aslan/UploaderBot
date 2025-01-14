c9c9a9ff35d2a531a7efe0e2144625c009bc7bb0
Taha-Hasemi <kiziltahaaslan@gmail.com>
2025-01-14 16:51:30 +0330

    feat: add photo dto

---
+++ Entities/Dtos/PhotoDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Entities.Dtos;
+
+public class PhotoDto : IDto
+{
+    public string FileId { get; set; } = null!;
+    public string FileUniqueId { get; set; } = null!;
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public int FileSize { get; set; }
+}
